ffb9be9e248c64d267dabcbef770c8f582b16bac
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2022-12-24 11:54:23 -0300

    Styling app header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,4 +4,34 @@ export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 5px;
+
+      color: ${(props) => props.theme['gray-100']};
+      text-decoration: none;
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+      transition: border 0.2s;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+    }
+  }
 `
