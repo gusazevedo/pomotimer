c5fd36edda332bbdd5fb474d9f52b7e04727b25d
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2022-12-24 09:36:33 -0300

    Setting up code linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./components/styles/global";
-import { defaultTheme } from "./components/styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './components/styles/global'
+import { defaultTheme } from './components/styles/themes/default'
 
 export function App() {
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <h1>Hello Pomo Timer</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello Pomo Timer</h1>
 
-            <GlobalStyle />
-        </ThemeProvider>
-    );
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
