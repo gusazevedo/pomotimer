46dd49cb6fc2d60510c8b28085489790c8f8099e
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2022-12-24 10:52:49 -0300

    Config application router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './components/styles/global'
-import { defaultTheme } from './components/styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Pomo Timer</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+
+      <nav>
+        <a href="#">timer</a>
+        <a href="#">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
